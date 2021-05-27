a55ac468b13cd3754ebebb9548141b7dee7436be
jason <jasoncx1986@gmail.com>
2021-05-27 14:50:15 +0800

    完成底部组件

--- src/components/NavFooter.vue
+++ src/components/NavFooter.vue
@@ -1,18 +1,44 @@
 <template>
   <div class="nav-footer">
-
+    <div class="logo">
+      <img src="/imgs/logo-footer.png" alt="" />
+      <p>无名商城</p>
+    </div>
+    <div><a href="https://bra.in/7pD8gr" class="net" target="_blank">我的动态知识网络</a></div>
+    <div class="link">Copyright ©2021 All Rights Reserved.</div>
   </div>
 </template>
 
 <script>
-
-
 export default {
-  name: 'nav-footer'
- 
-}
+  name: "nav-footer",
+};
 </script>
 
-<style>
+<style lang='scss'>
+.nav-footer {
+  width: 1919px;
+  height: 234px;
+  background: #333333;
+  border-top: 4px solid #ff7301;
+  text-align: center;
+  color: #999999;
+  font-size: 16px;
+  .logo {
+    margin-top: 46px;
+    margin-bottom: 31px;
+    img {
+      width: 53px;
+      height: 36px;
+      margin-bottom: 13px;
+    }
+  }
+  .net{
+    display: inline-block;
+      color: #999999;
+            margin-bottom: 13px;
 
+
+  }
+}
 </style>
