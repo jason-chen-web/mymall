31532771e7e7f9f664952a46f8d009187cbd12e2
jason <jasoncx1986@gmail.com>
2021-05-30 15:31:21 +0800

    完成轮播组件

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -2,6 +2,44 @@
   <div class="index">
     <div class="container">
       <div class="swiper-box">
+        <div class="nav-menu">
+          <ul class="menu-wrap">
+            <li class="menu-item">
+              <a href="javascript:;">手机 电话卡</a>
+              <div class="children">
+                <ul v-for="(item,i) of menuList" :key="i">
+                  <li v-for="(sub,j) of item " :key="j">
+                    <a :href="sub?'/#/product/'+sub.id:''">
+                      <img :src="sub?sub.img:'/imgs/item-box-1.png'" alt="" />
+                      {{sub?sub.name:'小米9'}}
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </li>
+            <li class="menu-item">
+              <a href="javascript:;">电视 盒子</a>
+            </li>
+            <li class="menu-item">
+              <a href="javascript:;">笔记本 平板</a>
+            </li>
+            <li class="menu-item">
+              <a href="javascript:;">家电 插线板</a>
+            </li>
+            <li class="menu-item">
+              <a href="javascript:;">出行 穿戴</a>
+            </li>
+            <li class="menu-item">
+              <a href="javascript:;">智能 路由器</a>
+            </li>
+            <li class="menu-item">
+              <a href="javascript:;">电源 配件</a>
+            </li>
+            <li class="menu-item">
+              <a href="javascript:;">生活 箱包</a>
+            </li>
+          </ul>
+        </div>
         <swiper :options="swiperOption">
           <swiper-slide v-for="(item, index) of slideList" :key="index">
             <a :href="'/#/product/' + item.id">
@@ -77,15 +115,114 @@ export default {
           img: "/imgs/slider/slide-5.jpg",
         },
       ],
+      menuList: [
+        [
+          {
+            id: 30,
+            img: "/imgs/item-box-1.png",
+            name: "小米CC9",
+          },
+          {
+            id: 31,
+            img: "/imgs/item-box-2.png",
+            name: "小米8青春版",
+          },
+          {
+            id: 32,
+            img: "/imgs/item-box-3.jpg",
+            name: "Redmi K20 Pro",
+          },
+          {
+            id: 33,
+            img: "/imgs/item-box-4.jpg",
+            name: "移动4G专区",
+          },
+        ],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+      ],
     };
   },
 };
 </script>
 
 <style lang='scss'>
+@import "../assets/scss/mixin.scss";
+@import "../assets/scss/config.scss";
 .index {
   .swiper-box {
+    .nav-menu {
+      width: 264px;
+      height: 451px;
+      position: absolute;
+      z-index: 10;
+      padding: 26px 0;
+      background: #55585a78;
+      box-sizing: border-box;
+      .menu-wrap {
+        .menu-item {
+          height: 50px;
+          line-height: 50px;
+          a {
+            font-size: 16px;
+            color: #ffffff;
+            padding-left: 30px;
+            display: block;
+            position: relative;
+            &:after {
+              position: absolute;
+              top: 17.5px;
+              right: 30px;
+              content: "";
+              @include bcg(10px, 15px, "/imgs/icon-arrow.png");
+            }
+          }
+          &:hover {
+            background: $colorA;
+            .children{
+              display: block;
+            }
+          }
+          .children{
+            display: none;
+            width: 964px;
+            height: 451px;
+            background-color: $colorG;
+            position: absolute;
+            left:264px;
+            top: 0;
+            ul{
+              display: flex;
+              justify-content: space-between;
+              height: 75px;
+              li{
+                height: 75px;
+                line-height: 75px;
+                flex: 1;
+                padding-left: 23px;
+                a{
+                  color: $colorB;
+                  font-size: 14px;
+                }
+                img{
+                  width: 42px;
+                  height: 35px;
+                  vertical-align: middle;
+                  margin-right: 15px;
+                }
+              }
+            }
+          }
+        }
+      }
+    }
     .swiper-container {
+      .swiper-button-prev {
+        left: 274px;
+      }
       height: 451px;
       img {
         width: 100%;
